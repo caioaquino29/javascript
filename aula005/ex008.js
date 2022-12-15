ee2f1d7280c2a55e8c20c9aa032ba8bab4b9bd10
caioaquino29 <115197086+caioaquino29@users.noreply.github.com>
2022-12-15 17:49:57 -0300

    Continuação do exercício
    Passaporte Brasileiro e Estrangeiro

---
+++ aula005/ex008.js
@@ -0,0 +1,6 @@
+var vel = 5000
+console.log('Seu valor em dolar é de '+vel+'US$')
+if(vel>1000){
+    console.log('Dá para comprar novo Iphone 14 Max ')
+}console.log('Não são bens material que trasem felicidade!!')
+
---
+++ aula005/ex009.js
@@ -0,0 +1,4 @@
+var pais= 'Brasil'
+console.log('Vivendo no '+pais)
+if (pais=="Brasil"){console.log('Você é Brsileiro(a)')}
+else{ console.log('Você é Extrangeiro(a)')}
