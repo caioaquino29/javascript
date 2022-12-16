03e6857a27e64fc37a88f1e8a0277efe666610dd
caioaquino29 <115197086+caioaquino29@users.noreply.github.com>
2022-12-16 14:33:52 -0300

    Exercitando
    é isso aí

---
+++ aula005/ex011.js
@@ -0,0 +1,11 @@
+var idade = 25
+console.log('Com idade de '+idade+' anos.')
+    if (idade < 16){
+        console.log('Não Vota. ')
+ }
+    else if ( idade<18||idade>65) {
+        console.log('Voto Opicinal.')}
+        else{ console.log('Volto obrigatório!')}
+   
+
+
