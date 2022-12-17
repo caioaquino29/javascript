753af8feed0c00bab93844bf55d50d5f81b2c36a
caioaquino29 <115197086+caioaquino29@users.noreply.github.com>
2022-12-16 21:21:11 -0300

    Exercício de fixação
    Muda com  as horas do dias

--- aula005/aula012/script.js
+++ aula005/aula012/script.js
@@ -1,1 +1,18 @@
-alert('olá')
+
+function data(){
+var inf=window.document.querySelector('.par')
+var img=window.document.querySelector("#foto")
+var tempo= new Date()
+var hora = 21 /* tempo.getHours()*/
+inf.textContent='Agora são '+hora+' hora'
+if(hora>=0 && hora<=12){
+img.src= "imagem/dia.png"
+document.body.style.backgroundColor="#D1D8D1"
+}else if (hora>=12 && hora<18){
+    img.src= "tarde.png"
+    document.body.style.backgroundColor="#A64037"
+
+}else {  img.src="noite.png"}
+
+document.body.style.backgroundColor="#011126"
+}
