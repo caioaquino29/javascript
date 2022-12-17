8ce371f9db6f3368c7b3c17650b054a63298d94e
caioaquino29 <115197086+caioaquino29@users.noreply.github.com>
2022-12-16 21:27:13 -0300

    maiiiiiiiiiis

--- aula005/aula012/script.js
+++ aula005/aula012/script.js
@@ -3,7 +3,7 @@ function data(){
 var inf=window.document.querySelector('.par')
 var img=window.document.querySelector("#foto")
 var tempo= new Date()
-var hora =  tempo.getHours()
+var hora = tempo.getHours ()
 inf.textContent='Agora são '+hora+' hora'
 if(hora>=0 && hora<=12){
 img.src= "imagem/dia.png"
@@ -12,7 +12,8 @@ document.body.style.backgroundColor="#D1D8D1"
     img.src= "tarde.png"
     document.body.style.backgroundColor="#A64037"
 
-}else {  img.src="noite.png"}
+}else {  img.src="noite.png" 
+document.body.style.backgroundColor="#011126"}
 
-document.body.style.backgroundColor="#011126"
+
 }
